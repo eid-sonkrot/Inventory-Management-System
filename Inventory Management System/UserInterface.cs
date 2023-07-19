dac540d151a88b45f0f085330b220711ebcd0a53
fdfd <eid.com>
2023-07-19 15:53:07 +0300

    Add project files.

---
+++ Inventory Management System/UserInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management_System
+{
+    internal class UserInterface
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
