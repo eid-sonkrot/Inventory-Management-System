bf45cd5db233d98f29c394e4de7f39d86ea4b45c
fdfd <eid.com>
2023-07-19 17:52:13 +0300

    Implement Interactive User Interface Functions In this commit, we lay the foundation for the project's interactive user interface by adding two crucial functions: 1.DisplayMenu: The DisplayMenu function presents users with a clear set of options, guiding them through various inventory management actions. These options include adding a product, viewing all products, editing a product, deleting a product, searching for a product, and exiting the system. 2. GetChoiceFromUser: The GetChoiceFromUser function ensures the user's choice is obtained correctly and validated. It prompts the user to enter a number representing their desired action. The input is then validated to ensure it falls within the valid range of 1 to 6, corresponding to the available menu options.

--- Inventory Management System/UserInterface.cs
+++ Inventory Management System/UserInterface.cs
@@ -10,7 +10,70 @@ namespace Inventory_Management_System
     {
         static void Main(string[] args)
         {
+            
 
+            while (true)
+            {
+                Console.WriteLine("Welcome to the Inventory Management System!");
+                DisplayMenu();
+                int choice = GetChoiceFromUser();
+
+                switch (choice)
+                {
+                    case 1:
+                        //  add a product to the inventory
+                        break;
+                    case 2:
+                        // display all products in the inventory
+                        break;
+                    case 3:
+                        // edit an existing product in the inventory
+                        break;
+                    case 4:
+                        //  remove a product from the inventory
+                        break;
+                    case 5:
+                        //search for a product in the inventory
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Console.WriteLine("Exiting the Inventory Management System. Goodbye!");
+                        System.Threading.Thread.Sleep(1000);
+                        return;
+                    
+                        
+                }
+                Console.Clear();
+            }
         }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("\nOptions:");
+            Console.WriteLine("1. Add a product");
+            Console.WriteLine("2. View all products");
+            Console.WriteLine("3. Edit a product");
+            Console.WriteLine("4. Delete a product");
+            Console.WriteLine("5. Search for a product");
+            Console.WriteLine("6. Exit");
+        }
+
+        static int GetChoiceFromUser()
+        {
+            Console.Write("Enter your choice (1-6): ");
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 6)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Inventory Management System!");
+                DisplayMenu();
+                Console.Write("Invalid input. Enter a valid choice (1-6): ");
+
+            }
+            return choice;
+        }
+
+
     }
+    
 }
