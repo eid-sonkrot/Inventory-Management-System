9551c5d30eaa369e62c7b4722e6c700579361a5c
fdfd <eid.com>
2023-07-31 15:44:25 +0300

    Refactor code to comply with the style guide
    In this commit, we made several improvements to the code to adhere to the "cartera" style guide. The changes include:
    
    1. Removing extra spaces for a cleaner code layout.
    2. Converting all methods (except Main) to non-static methods for better object-oriented design.
    3. Enhancing code readability by organizing the switch statement into a separate method and handling invalid input gracefully.
    
    These changes improve the overall maintainability and readability of the codebase, making it easier for future development and collaboration.

--- Inventory Management System/UserInterface.cs
+++ Inventory Management System/UserInterface.cs
@@ -1,53 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Inventory_Management_System
 {
-    internal class UserInterface
+    public class UserInterface
     {
         static void Main(string[] args)
         {
-            
-
+            UserInterface userInterface = new UserInterface();
+             
             while (true)
             {
                 Console.WriteLine("Welcome to the Inventory Management System!");
-                DisplayMenu();
-                int choice = GetChoiceFromUser();
-
-                switch (choice)
-                {
-                    case 1:
-                        //  add a product to the inventory
-                        break;
-                    case 2:
-                        // display all products in the inventory
-                        break;
-                    case 3:
-                        // edit an existing product in the inventory
-                        break;
-                    case 4:
-                        //  remove a product from the inventory
-                        break;
-                    case 5:
-                        //search for a product in the inventory
-                        break;
-                    case 6:
-                        Console.Clear();
-                        Console.WriteLine("Exiting the Inventory Management System. Goodbye!");
-                        System.Threading.Thread.Sleep(1000);
-                        return;
-                    
-                        
-                }
+                userInterface.DisplayMenu();
+                var choice = userInterface.GetChoiceFromUser();
+                userInterface.HandleInventoryAction(choice);
                 Console.Clear();
             }
         }
-
-        static void DisplayMenu()
+        public void DisplayMenu()
         {
             Console.WriteLine("\nOptions:");
             Console.WriteLine("1. Add a product");
@@ -57,12 +26,12 @@ namespace Inventory_Management_System
             Console.WriteLine("5. Search for a product");
             Console.WriteLine("6. Exit");
         }
-
-        static int GetChoiceFromUser()
+        public int GetChoiceFromUser()
         {
             Console.Write("Enter your choice (1-6): ");
-            int choice;
-            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 6)
+            var choice =0;
+            var isValidInput = int.TryParse(Console.ReadLine(), out  choice);
+            while (!isValidInput || choice < 1 || choice > 6)
             {
                 Console.Clear();
                 Console.WriteLine("Welcome to the Inventory Management System!");
@@ -72,8 +41,31 @@ namespace Inventory_Management_System
             }
             return choice;
         }
-
-
+        public void HandleInventoryAction(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    //  add a product to the inventory
+                    break;
+                case 2:
+                    // display all products in the inventory
+                    break;
+                case 3:
+                    // edit an existing product in the inventory
+                    break;
+                case 4:
+                    //  remove a product from the inventory
+                    break;
+                case 5:
+                    //search for a product in the inventory
+                    break;
+                case 6:
+                    Console.Clear();
+                    Console.WriteLine("Exiting the Inventory Management System. Goodbye!");
+                    System.Threading.Thread.Sleep(1000);
+                    return;
+            }
+        }
     }
-    
 }
