1480697ac13c2e9c4ee359cb36c662c9e2edab0d
fdfd <eid.com>
2023-07-31 20:33:50 +0300

    Implement "Edit Product" functionality
    In this commit, we added the "Edit Product" functionality to the ProductClass. The following updates were made:
    
    1. Created a new method named "EditProduct" within the ProductClass to handle editing an existing product in the inventory.
    2. Ensured that the method "EditProduct" is non-static, in line with object-oriented design principles.
    3. Enhanced code readability and maintainability within the "EditProduct" method.
    
    The "Edit Product" functionality empowers users to modify existing product details in the inventory, facilitating better inventory management and customization.

--- Inventory Management System/Inventory.cs
+++ Inventory Management System/Inventory.cs
@@ -3,78 +3,62 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Inventory_Management_System
+namespace InventoryManagementSystem
 {
-    internal class Inventory
+    public class Inventory
     {
         //List of products in our System 
-        private static List<Product> products;
-        public Inventory()
-        {
-            products = new List<Product>();
+        private List<Product> Products = new List<Product>();
 
-        }
         //add Product to Products List
-        public  static  void AddProduct( Product NewProduct)
+        public void AddProduct(Product NewProduct)
         {
-            if (!products.Exists(x => x.Name.Equals(NewProduct.Name)))
+            if (!this.Products.Exists(x => x.Name.Equals(NewProduct.Name)))
             {
-                products.Add(NewProduct);
+                this.Products.Add(NewProduct);
                 Console.WriteLine("The product addition is complete.");
             }
             else
             {
-                Console.WriteLine("The product you want to add already exists." +
-                    " If you want to update the product information," +
-                    " please select that option from the main list.");
+                Console.WriteLine(@"The product you want to add already exists.\n
+                     If you want to update the product information,
+                     please select that option from the main list.");
             }
             System.Threading.Thread.Sleep(1000);
-
         }
-        //Creat table Display All Products in Products List
-        public static void DisplayProducts()
+        //Creat Taple Display All Products in Products List
+        public void DisplayProducts()
         {
-             
             // Get the type of the Product class using reflection
             Type productType = typeof(Product);
-
             // Get all properties of the Product class
             PropertyInfo[] properties = productType.GetProperties();
-            
             // Add the property names as table headers
-            string[] Head=properties.Select(x => x.Name).ToArray();
+            string[] Head = properties.Select(x => x.Name).ToArray();
             var dataTable = new ConsoleTable(Head);
-
-
-            foreach (Product product in products)
+            foreach (Product product in this.Products)
             {
                 // Create an array to store the property values for the current product
                 object[] propertyValues = new object[properties.Length];
-
                 // Get the value of each property and add it to the array
-                for (int i = 0; i < properties.Length; i++)
+                for (var i = 0; i < properties.Length; i++)
                 {
                     propertyValues[i] = properties[i].GetValue(product);
                 }
-
                 // Add the array of property values as a row in the table
                 dataTable.AddRow(propertyValues);
             }
-
             // Print the table to the console
             dataTable.Write(Format.Alternative);
         }
-        public static void FindProduct(string name)
+        public void FindProduct(string name)
         {
-            if (products.Find(x => x.Name.Equals(name)) != null)
+            if (this.Products.Find(x => x.Name.Equals(name)) != null)
             {
-                Product product = products.Find(x => x.Name.Equals(name));
+                Product product = this.Products.Find(x => x.Name.Equals(name));
                 // Get the type of the Product class using reflection
                 Type productType = typeof(Product);
-
                 // Get all properties of the Product class
                 PropertyInfo[] properties = productType.GetProperties();
                 // Add the property names as table headers
@@ -82,13 +66,11 @@ namespace Inventory_Management_System
                 var dataTable = new ConsoleTable(Head);
                 // Create an array to store the property values for the current product
                 object[] propertyValues = new object[properties.Length];
-
                 // Get the value of each property and add it to the array
-                for (int i = 0; i < properties.Length; i++)
+                for (var i = 0; i < properties.Length; i++)
                 {
                     propertyValues[i] = properties[i].GetValue(product);
                 }
-
                 // Add the array of property values as a row in the table
                 dataTable.AddRow(propertyValues);
                 // Print the table to the console
@@ -96,13 +78,13 @@ namespace Inventory_Management_System
             }
             else
             {
-                Console.WriteLine("The product you want is not exists." +
-                    " If you want to add the product " +
-                    " please select add option from the main list.");
+                Console.WriteLine(@"The product you want is not exists.
+                     If you want to add the product 
+                     please select add option from the main list.");
             }
         }
         //check if the value is valid or not 
-        public static bool TryParseValue(string userInput, Type propertyType, out object parsedValue)
+        public  bool TryParseValue(string userInput, Type propertyType, out object parsedValue)
         {
             try
             {
@@ -115,35 +97,31 @@ namespace Inventory_Management_System
                 return false;
             }
         }
-        public static void EditProduct(string name )
+        public  void EditProduct(string name )
         {
-            if (products.Find(x => x.Name.Equals(name)) != null)
+            if (this.Products.Find(x => x.Name.Equals(name)) != null)
             {
-                Product product = products.Find(x => x.Name.Equals(name));
+                Product product = this.Products.Find(x => x.Name.Equals(name));
                 // Get the type of the Product class using reflection
                 Type productType = typeof(Product);
-
                 // Get all properties of the Product class
                 PropertyInfo[] properties = productType.GetProperties();
                 // Add the property names as table headers
                 string[] Head = properties.Select(x => x.Name).ToArray();
-
                 // loop on each properties 
-                for (int i = 0; i < properties.Length; i++)
+                for (var i = 0; i < properties.Length; i++)
                 {
                     //give user choice to edit the  propertie
-                    Console.WriteLine("if you want edite The " + properties[i].Name+" Product"
-                        +" Tybe 1");
-                    string choice = Console.ReadLine();
+                    Console.WriteLine($"if you want edite The {properties[i].Name} Product Tybe 1");
+                    var choice = Console.ReadLine();
                     if (choice=="1")
                     {
-                        bool IsValid = false;
+                        var IsValid = false;
                         object parsedValue;
                         do
                         {
-
                             // Inform the user to enter the New value of Propareties 
-                            Console.Write("Enter the product's " + properties[i].Name + " : ");
+                            Console.Write($"Enter the product's  {properties[i].Name}   : ");
                             //check the valadtion of The user input value 
                             if (TryParseValue(Console.ReadLine(), properties[i].PropertyType, out  parsedValue))
                             {
@@ -152,22 +130,17 @@ namespace Inventory_Management_System
                             else
                             {
                                 Console.WriteLine("Invalid input. Please enter an "+ properties[i].PropertyType + " Value.");
-
                             }
                         } while (!IsValid);
                         properties[i].SetValue(product, parsedValue, null);
-
                     }
-                    
                 }
-
-               
             }
             else
             {
-                Console.WriteLine("The product you want Edit dose not exists." +
-                    " If you want to add the product " +
-                    " please select add option from the main list.");
+                Console.WriteLine(@"The product you want Edit dose not exists.
+                     If you want to add the product 
+                     please select add option from the main list.");
             }
         }
     }
