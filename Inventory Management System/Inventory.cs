e445203db7262d7598ed9eb4a40dd7c82e24efb4
fdfd <eid.com>
2023-07-31 11:13:30 +0300

    Refactor code style and filenames to PascalCase
    - Updated code style to follow PascalCase naming convention for variables, functions, and classes.
    - Renamed files and directories to PascalCase to improve consistency.
    - Removed unnecessary library calls to optimize code and reduce dependencies.

--- Inventory Management System/Inventory.cs
+++ Inventory Management System/Inventory.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Inventory_Management_System
+namespace InventoryManagementSystem
 {
     internal class Inventory
     {
--- Inventory Management System/Product.cs
+++ Inventory Management System/Product.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Inventory_Management_System
+namespace InventoryManagementSystem
 {
-    internal class Product
+    public class Product
     {
     }
 }
