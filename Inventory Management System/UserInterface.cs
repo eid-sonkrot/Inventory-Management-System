0f10dbd51c00941c1552c7aea7111966e392ca53
eid-sonkrot <129992917+eid-sonkrot@users.noreply.github.com>
2023-08-06 09:21:41 +0000

    Refactor code style

--- Inventory Management System/UserInterface.cs
+++ Inventory Management System/UserInterface.cs
@@ -46,6 +46,7 @@ namespace InventoryManagementSystem
         public void HandleInventoryAction(int choice)
         {
             Inventory Inventory = new Inventory();
+            
             switch (choice)
             {
                 case 1:
