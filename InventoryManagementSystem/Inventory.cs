6d66935b16e02b5d570e83ddc0c4e6c18f74f730
eid-sonkrot <129992917+eid-sonkrot@users.noreply.github.com>
2023-08-07 21:26:19 +0000

    in this commit we reduce the DispalyProduct method

--- InventoryManagementSystem/Inventory.cs
+++ InventoryManagementSystem/Inventory.cs
@@ -31,25 +31,7 @@ namespace InventoryManagementSystem
         //Creat Taple Display All Products in Products List
         public  void DisplayProducts()
         {
-            // Get the type of the Product class using reflection
-            var productType = typeof(Product);
-            // Get all properties of the Product class
-            var properties = productType.GetProperties();
-            // Add the property names as table headers
-            var head=properties.Select(x => x.Name).ToArray();
-            var dataTable = new ConsoleTable(head);
-            foreach (Product product in Products)
-            {
-                // Create an array to store the property values for the current product
-                var propertyValues = new object[properties.Length];
-                // Get the value of each property and add it to the array
-                for (var i = 0; i < properties.Length; i++)
-                {
-                    propertyValues[i] = properties[i].GetValue(product);
-                }
-                // Add the array of property values as a row in the table
-                dataTable.AddRow(propertyValues);
-            }
+            var dataTable = new ConsoleTable(this.Products);
             // Print the table to the console
             dataTable.Write(Format.Alternative);
         }
